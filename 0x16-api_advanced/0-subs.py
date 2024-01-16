5eb7c1eecbf0393b2d652e8aed5967243419b913
Mikenziokah <mikenziokah@gmail.com>
2024-01-16 19:16:46 +0000

    reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API and returns the number of subscribers"""
-
+""" Queries the Reddit API and returns the number of subscribers """
+import json
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """ Function that queries the Reddit API """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    """ Finds number of subscribers of an specific users """
+    try:
+        req = requests.get(
+            'https://www.reddit.com/r/' + sys.argv[1] + '/about.json',
+            allow_redirects=False,
+            headers={'User-agent': 'Baquero28'})
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
-    else:
-        return 0
+        dict_reddit = json.loads(req.text)
+        data_key = dict_reddit['data']
+
+        return (data_key['subscribers'])
+    except:
+        return (0)
